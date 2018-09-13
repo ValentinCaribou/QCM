34359a143793638a33a582156c11114e442d7829
JulienChollet <julien.chollet@yahoo.fr>
2018-09-13 12:09:29 +0200

    ServletFormateur, vues associées

---
+++ src/fr/eni/ecole/servlets/ServletCreationTheme.java
@@ -0,0 +1,29 @@
+package fr.eni.ecole.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletCreationTheme", urlPatterns = "/theme")
+public class ServletCreationTheme extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/creationTheme.jsp").forward(request, response);
+    }
+}
---
+++ web/WEB-INF/menuFormateur.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrateur
+  Date: 12/09/2018
+  Time: 14:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<ol class="list-group">
+    <li class="list-group-item list-group-item-action"><a class="menuFormateurLien" href="${pageContext.request.contextPath}/formateur">Retour en page d'accueil</a></li>
+    <li class="list-group-item list-group-item-action"><a class="menuFormateurLien" href="${pageContext.request.contextPath}/theme">Création, modification thèmes</a></li>
+    <li class="list-group-item list-group-item-action"><a class="menuFormateurLien" href="${pageContext.request.contextPath}/question">Création, modification questions</a></li>
+    <li class="list-group-item list-group-item-action"><a class="menuFormateurLien" href="${pageContext.request.contextPath}/test" >Création, modification testes</a></li>
+</ol>
+
+
