f1ca94fd928b54b0574ffa9f7864545d77e3d8cb
JulienChollet <julien.chollet@yahoo.fr>
2018-09-14 09:49:51 +0200

    Update Contants

--- src/fr/eni/ecole/constantes/ConstantesSql.java
+++ src/fr/eni/ecole/constantes/ConstantesSql.java
@@ -7,9 +7,9 @@ public class ConstantesSql {
     public static final String connexionQuery = "SELECT idUtilisateur, nom, prenom, email, password, codeProfil, codePromo FROM [BDD_QCM].[dbo].[UTILISATEUR] WHERE email = ?";
 
 
-    public static final String themeCreate = "INSERT INTO [BDD_QCM].[dbo].[THEME](idTheme, libelle) VALUES ?,?";
-    public static final String themeQuery = "SELECT  libelle FROM [BDD_QCM].[dbo].[THEME]";
-    public static final String getThemeUpdate = "UPDATE idTheme, libelle WHERE idTheme = ?";
-    public static final String themeDelete = "DELETE IdTheme, libelle FROM [BDD_QCM].[dbo].[THEME] WHERE idTheme =  ?";
+    public static final String themeCreate = "INSERT INTO [BDD_QCM].[dbo].[THEME](idTheme, libelle) VALUES (?,?)";
+    public static final String themeQuery = "SELECT libelle FROM [BDD_QCM].[dbo].[THEME]";
+    public static final String getThemeUpdate = "UPDATE [BDD_QCM].[dbo].[THEME] SET libelle = ? WHERE idTheme = ?";
+    public static final String themeDelete = "DELETE FROM [BDD_QCM].[dbo].[THEME] WHERE idTheme =  ?";
 }
 
